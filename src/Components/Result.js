98c3698a47f52fb8cacc9873c8106c6aa388542c
hyunchanpark <eye3570@gmail.com>
2021-08-17 01:23:19 +0900

    Style : Result & Timer 스타일 추가
    Co-authored-by: daeun-react <daeun-react@users.noreply.github.com>

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Result = ({ value }) => {
   return (
-    <div>
-      <p>{value || '정렬할 입력값을 넣어주세요'}</p>
-    </div>
+    <Wrapper>
+      <p>{value || '숫자를 입력해주세요.'}</p>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  margin-bottom: 20px;
+  padding: 0px 32px;
+
+  p {
+    padding: 32px 10px;
+    border: 1px solid ${({ theme }) => theme.color.gray};
+    border-radius: 4px;
+  }
+`;
 
 export default Result;
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { dateLocalFunc } from 'Utils/date';
 
 const Timer = ({ type }) => {
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -12,10 +13,28 @@ const Timer = ({ type }) => {
     return () => {
       clearInterval(timer);
     };
-  }, [date]);
+  }, []);
 
-  // local ? ko-KR : en-US 에 따른 조건 처리
-  return <div>{dateLocalFunc(date, type)}</div>;
+  return (
+    <Wrapper type={type}>
+      <h2>{dateLocalFunc(date, type)}</h2>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: 48px 32px;
+  border-bottom: ${({ type, theme }) =>
+    type === 'KR' && `1px solid ${theme.color.lightGray}`};
+
+  border-top: ${({ type, theme }) =>
+    type === 'US' && `1px solid ${theme.color.lightGray}`};
+
+  h2 {
+    font-size: 20px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.color.black};
+  }
+`;
 
 export default Timer;
