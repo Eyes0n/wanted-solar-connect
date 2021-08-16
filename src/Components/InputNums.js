7eed71ed4e74e488a96baa6e022108835756ed1d
hyunchanpark <eye3570@gmail.com>
2021-08-17 01:21:28 +0900

    Feat : forwardRef로 input 입력 불가 & input 초기화 기능 추가
    Co-authored-by: daeun-react <daeun-react@users.noreply.github.com>

--- src/Components/InputNums.js
+++ src/Components/InputNums.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
 
-const InputNums = ({ value, onChange }) => {
+const InputNums = forwardRef(({ value, onChange, onKeyPress }, ref) => {
   return (
-    <div>
-      <input value={value} onChange={onChange} />
-    </div>
+    <Wrapper>
+      <label htmlFor="input">숫자 입력 :</label>
+      <input
+        id="input"
+        ref={ref}
+        disabled={false}
+        placeholder="숫자를 입력해주세요."
+        value={value}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+    </Wrapper>
   );
-};
+});
+
+const Wrapper = styled.div`
+  display: flex;
+  margin-top: 20px;
+
+  label {
+    margin-left: 32px;
+    padding: 12px 0;
+  }
+
+  input {
+    flex: 1;
+    padding: 12px;
+    margin-left: 12px;
+    margin-right: 32px;
+    border: 1px solid ${({ theme }) => theme.color.gray};
+    border-radius: 4px;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.color.gray};
+    }
+  }
+`;
 
 export default InputNums;
