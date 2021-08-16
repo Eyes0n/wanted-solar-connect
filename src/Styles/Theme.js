23a36dd3a35fd00736402985fa1101306f29f8f9
hyunchanpark <eye3570@gmail.com>
2021-08-17 01:22:08 +0900

    Style : theme 색상 추가
    Co-authored-by: daeun-react <daeun-react@users.noreply.github.com>

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,5 +1,10 @@
 export const Theme = {
   color: {
-    gray: '#9B9494',
+    black: '#282c37',
+    gray: '#9baec8',
+    lightGray: '#d9e1e8',
+    blue: '#2b90d9',
+    darkBlue: '#4F86C6',
+    red: '#f94e3f',
   },
 };
